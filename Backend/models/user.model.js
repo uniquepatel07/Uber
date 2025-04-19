f19faebfd0fc7def4e9d30ff79b036545398440f
uniquepatel01 <uniquepatel01@gmail.com>
2025-04-19 23:42:22 +0530

    Created Services and Controllers

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -33,12 +33,12 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-userSchema.method.generateAuthToken = function(){
+userSchema.methods.generateAuthToken = function(){
     const token = jwt.sign({_id:this._id}, process.env.JWT_SECRET)
     return token;
 }
 
-userSchema.method.comparePassword = async function(password) {
+userSchema.methods.comparePassword = async function(password) {
     return await bcrypt.compare(password, this.password);
 }
 
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,10 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const {body} = require("express-validator")
+const userController = require('../controllers/user.controller');
+
+router.post('/register', [
+    body('email').isEmail().withMessage("Invalid Email "),
+    body('fullname.firstname').isLength({min:3}).withMessage("first name must be atleast three character long"),
+    body('password').isLength({min:6}).withMessage("password must be atlast six letter")
+], userController.registerUser)
 
 
 
-
-
-
+ 
 module.exports = router;
